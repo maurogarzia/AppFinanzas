16af55f86a1a96c736e06566ccb81c863991dfaf
maurogarzia <maurogarzia2@gmail.com>
2025-10-11 20:54:32 -0300

    I add stores

---
+++ AppFinanzas/src/store/useStoreMovents.ts
@@ -0,0 +1,77 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+import type { IMovents } from '../types/IMovents'
+
+interface IUseStoreMovents {
+    movents : IMovents[]
+    activeMovent : IMovents | null
+    listExpenses : IMovents[] | []
+    listIncome : IMovents[] | []
+    listAncient : IMovents[] | []
+    listRecent : IMovents[] | []
+
+    setActiveMovent: (incommingMovent : IMovents | null) => void
+    addMovents : (newMovent : IMovents) => void
+    deleteMovents: (id: string) => void,
+    editMovents: (id: string, newMovent : IMovents) => void
+    setListExpenses : VoidFunction,
+    setLisIncome : VoidFunction
+    setListRecent: VoidFunction
+    setListAncient : VoidFunction
+}
+
+export const useStoreMovents = create<IUseStoreMovents>()(
+    persist((set) => ({
+            movents : [],
+            activeMovent : null,
+            listExpenses : [],
+            listIncome : [],
+            listAncient: [],
+            listRecent: [],
+
+            setActiveMovent: (incommingMovent) => set({activeMovent : incommingMovent}),
+
+            addMovents : (newMovent) => set((state) => ({
+                movents : [...state.movents, newMovent],
+            })),
+
+            deleteMovents : (id) => set((state) => ({
+                movents : state.movents.filter((f) => (
+                    f.id !== id
+                ))
+            })),
+
+
+            editMovents: (id, newMovent) => set((state) => ({
+                movents : state.movents.map((m) => (
+                    m.id === id ? newMovent : m
+                ))
+            })),
+
+            setLisIncome : () => set((state) => ({
+                listIncome : state.movents.filter((i) => i.type === "ingreso")
+            })),
+
+            setListExpenses : () => set((state) => ({
+                listExpenses : state.movents.filter((i) => i.type === "gasto")
+            })),
+
+            setListAncient: () => set((state) => ({
+                listAncient : state.movents.slice().sort((a,b) => {
+                    return new Date(a.date).getTime() - new Date(b.date).getTime()
+                })
+            })),
+
+            setListRecent : () => set((state) => ({
+                listRecent : state.movents.slice().sort((a,b) => {
+                    return new Date(b.date).getTime() - new Date(a.date).getTime()
+                })
+            }))
+        }),
+        {
+            name: 'user-storage', 
+            storage: createJSONStorage(() => AsyncStorage)
+        }
+    )
+)
