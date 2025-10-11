99ed87527c2c49cb8a619a84b47f1cd404c9e17f
maurogarzia <maurogarzia2@gmail.com>
2025-10-11 20:59:38 -0300

    fix to App.tsx

--- AppFinanzas/App.tsx
+++ AppFinanzas/App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Header } from './src/components/Header/Header';
+import { Home } from './src/components/Home/Home';
+import { AddMovents } from './src/components/AddMovents/AddMovents';
+import useStoreModal from './src/store/useStoreModal';
+
 
 export default function App() {
+  const { view } = useStoreModal();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <StatusBar style='light'/>
+      
+      <Header/>
+      <ScrollView style={styles.container}>
+        <Home/>
+      </ScrollView>
+
+      {view && (
+        <View style={styles.modalBackdrop}>
+          <AddMovents/>
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +30,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  modalBackdrop: {
+    position: 'absolute',   
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#0000007c',  
+    justifyContent: 'center',      
+    alignItems: 'center',          
+    zIndex: 999,
   },
 });
